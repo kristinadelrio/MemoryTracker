e53c7d51a324dd06407fcf9f40a119f9beec578d
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-08-30 20:59:58 +0300

    Initial Commit

---
+++ MemoryTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryTracker
+//
+//  Created by Alejandro Del Rio Albrechet on 8/30/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
