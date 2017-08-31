ab903091e151f72c880a27d39bfa2e1b4f0f4f2d
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-08-31 16:38:43 +0300

    Added card view

--- MemoryTracker/CardView.swift
+++ MemoryTracker/CardView.swift
@@ -6,4 +6,28 @@
 //
 //
 
-import Foundation
+import UIKit
+
+class CardView: UIImageView {
+    let cardBack = #imageLiteral(resourceName: "card")
+    var cardFace: UIImage?
+    
+    // Makes card back visible
+    func setCardBack() {
+        setImageAttributes(mode:  .scaleToFill, backColor: UIColor.white, userInteraction: true)
+        image = cardBack
+    }
+    
+    // Makes card face visible
+    func setCardFace() {
+        setImageAttributes(mode:  .scaleAspectFit, backColor: UIColor.clear, userInteraction: true)
+        image = cardFace
+    }
+    
+    // Sets attributes for image
+    func setImageAttributes(mode: UIViewContentMode, backColor: UIColor, userInteraction: Bool) {
+        isUserInteractionEnabled = userInteraction
+        backgroundColor = backColor
+        contentMode = mode
+    }
+}
