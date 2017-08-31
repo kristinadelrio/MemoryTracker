b0aa36c1b4d8c8d058928084fbf1d40790fe1b10
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-08-31 16:39:20 +0300

    Added image suffle manager

--- MemoryTracker/MapManager.swift
+++ MemoryTracker/MapManager.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+class MapManager {
+    var imageName = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
+    
+    init() {
+        shuffleImages()
+    }
+    
+    func shuffleImages() {
+        imageName.sort(by: { (_,_) in arc4random() < arc4random() })
+    }
+}
