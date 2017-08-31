7120249178add68f407c78964f0ea562701ff5a0
Kristina Del Rio Albrechet <kabrechet@gmail.com>
2017-08-31 16:15:25 +0300

    Added card back

---
+++ MemoryTracker/CardView.swift
@@ -0,0 +1,9 @@
+//
+//  CardView.swift
+//  MemoryTracker
+//
+//  Created by Alejandro Del Rio Albrechet on 8/31/17.
+//
+//
+
+import Foundation
---
+++ MemoryTracker/MapManager.swift
@@ -0,0 +1,9 @@
+//
+//  MapManager.swift
+//  MemoryTracker
+//
+//  Created by Alejandro Del Rio Albrechet on 8/31/17.
+//
+//
+
+import Foundation
